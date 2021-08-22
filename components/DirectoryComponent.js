cc76542eb59f624936460c551fc5efb69ef36f4f
illfriday <willprouty@gmail.com>
2021-08-21 22:36:00 -0700

    messed up expo

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-export default function Directory(props) {
-  const renderDirectoryItem = ({ item }) => {
+export default class Directory extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      campsites: CAMPSITES,
+      selectedCampsite: null,
+    };
+  }
+
+  render() {
+    const renderDirectoryItem = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          onPress={() => props.onPress(item.id)}
+          leftAvatar={{ source: require("./images/react-lake.jpg") }}
+        />
+      );
+    };
+
     return (
-      <ListItem
-        title={item.name}
-        subtitle={item.description}
-        leftAvatar={{ source: require("./images/react-lake.jpg") }}
-      />
+      <FlatList
+        data={props.campsites}
+        renderItem={renderDirectoryItem}
+        keyExtractor={(item) => item.id.toString()}
+      ></FlatList>
     );
-  };
-
-  return (
-    <FlatList
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={(item) => item.id.toString()}
-    ></FlatList>
-  );
+  }
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+<<<<<<< Updated upstream
 import { View } from "react-native";
 import { CAMPSITES } from "../shared/campsites";
 import Directory from "./DirectoryComponent";
@@ -14,6 +15,46 @@ export default class Main extends Component {
     return (
       <View>
         <Directory campsites={this.state.campsites} />
+=======
+import { View, Platform } from "react-native";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Constants from "expo-constants";
+import Directory from "./DirectoryComponent";
+
+const DirectoryNavigator = createStackNavigator(
+  {
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    initialRoute: "Directory",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+        }}
+      >
+        <AppNavigator />
+>>>>>>> Stashed changes
       </View>
     );
   }
