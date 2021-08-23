f7c5880d87efd2d3cca2440ac88d944b1a91e772
illfriday <willprouty@gmail.com>
2021-08-23 16:04:02 -0700

    add drawer navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
+import Home from "./HomeComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { View, Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import Constants from "expo-constants";
 
-
 const DirectoryNavigator = createStackNavigator(
-
   //Route Configs object, the only needed object in createStackNavigator
   {
     Directory: { screen: Directory },
@@ -28,8 +28,34 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator },
+  },
+  {
+    drawerBackgroundColor: "#CEC8FF",
+  }
+);
 //wrap TOP LEVEL NAVIGATOR in createAppContainer
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
   render() {
@@ -41,7 +67,6 @@ class Main extends Component {
         }}
       >
         <AppNavigator />
-
       </View>
     );
   }
