7a0d3969fb9a84aba8ea69ad7b61267e15f1777d
illfriday <willprouty@gmail.com>
2021-09-10 19:06:04 -0700

    add verification alert to deleteFavorite

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { FlatList, View, Text, StyleSheet, Alert } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -32,7 +32,25 @@ class Favorites extends Component {
           <View style={styles.deleteView}>
             <TouchableOpacity
               style={styles.deleteTouchable}
-              onPress={() => this.props.deleteFavorite(item.id)}
+              onPress={() =>
+                Alert.alert(
+                  "Delete Favorite?",
+                  `Are ou sure you wish to delete the favored campsite ${item.name}?`,
+                  [
+                    {
+                      text: "Cancel",
+                      onPress: () =>
+                        console.log(console.log(`${item.name} Not Deleted`)),
+                      style: "cancel",
+                    },
+                    {
+                      text: "OK",
+                      onPress: () => this.props.deleteFavorite(item.id),
+                    },
+                  ],
+                  { cancelable: false }
+                )
+              }
             >
               <Text style={styles.deleteText}>Delete</Text>
             </TouchableOpacity>
